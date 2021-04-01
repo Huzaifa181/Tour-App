7579eff52a92d3d0978b5487a9e3e5f246c7e800
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-04-01 13:43:22 +0500

    Get the Tour with Better Filtering Pagination Limiting and Sorting

--- Controller/tours.js
+++ Controller/tours.js
@@ -12,7 +12,7 @@ const getTourByUser=async (req,res,next)=>{
         identifiedTour=await Tour.find({creator:id})
     }
     catch(err){
-        const error=new httpError("Fatching TOurs Failed, Could not find Tour",401)
+        const error=new httpError("Fatching Tours Failed, Could not find Tour",401)
         return next(error)
     }
     if(!identifiedTour || identifiedTour.length==0){
@@ -31,11 +31,12 @@ const getParticularTour=async (req,res,next)=>{
         identifiedTour=await Tour.findById(id)
     }
     catch(err){
+        
         const error=new httpError("Something went wrong, Could not Find Tour",500)
         return next(error)
     }
     if(!identifiedTour){
-        const error=new httpError("Could not Find Tour fro the particular id",404)
+        const error=new httpError("Could not Find Tour for the particular id",404)
         return next(error)
     }
     res.status(200).json({
@@ -45,8 +46,54 @@ const getParticularTour=async (req,res,next)=>{
 
 const getAllTours=async (req,res,next)=>{
     let data
+    // 1) filtering
+    let queryObj={...req.query}
+    const excludedFields=["sort",'page','fields','limit']
+    excludedFields.forEach(el=>delete queryObj[el])
+
+    //Advanced Filtering
+    let queryStr=JSON.stringify(queryObj)
+    queryStr=queryStr.replace(/\b(gt|gte|lt|lte)\b/,match=>`$${match}`)
+    //So in API (http://api/tours?duration[lte]=34)
+    console.log(JSON.parse(queryStr))
     try{
-        data=await Tour.find().exec();
+        data=await Tour.find(JSON.parse(queryStr));
+        //2) Sorting
+        if(req?.query?.sort){
+            const sortBy=req.query.sort.split(',').join(' ')
+            //So in API (http://api/tours?sort=price,ratingAverage)
+            //if 2 doc have same price so it will sort acc to ratingAverage
+            //By default this will sort in ascending order
+            data=data.sort(sortBy)
+        }else{
+            data=data.sort('-createdAt')
+            //This will sort according to the descending order
+        }
+        // 3) Fields Limiting: Means Show which fields in the document to res
+        if(req?.query?.sort){
+            const fields=req.query.fields.split(',').join(" ")
+            data=data.select(fields)
+            // So in the API http://tours?fields=price, -duration
+            // price means including and -duration means excluding to show the res to users
+            // You can also build this in the schema by type "select=false"
+        }
+        else{
+            query=query.select('-__v')
+        }
+
+        //4) Pagination
+        const page=req.query.page * 1 || 1;
+        const limit=req.query.limit* 1 || 10;
+        const skip=(page-1)*limit
+
+        data=data.skip(skip).limit(limit)
+        // So in the API http://tours?page=3&limit=3
+        if(req.query.page){
+            const numTour=await Tour.countDocuments();
+            if(skip>numTour){
+                throw new Error("Could Not Find That Page")
+            }
+        }
     }
     catch(err){
         const error=new httpError("Could not Find Tours",500)
@@ -56,20 +103,15 @@ const getAllTours=async (req,res,next)=>{
         const error=new httpError("No Tour",201)
         return next(error)
     }
+    const tours=await data
     res.status(201)
     res.json({
-        message:"Successful",
-        data:data
+        status:"Success",
+        data:tours
     })
 }
 
 const createTour=async (req,res,next)=>{
-    // const error=validationResult(req)
-    // const {title,description,image,address,location}=req.body
-    // if(!error.isEmpty()){
-    //     const error= new httpError("Invalid Input Passed, Please Check your Data",422)
-    //     return next(error)
-    // }
     let createTour
     try{
         createTour=new Tour(req.body)
@@ -80,32 +122,6 @@ const createTour=async (req,res,next)=>{
             message:'Invalid Data Sent'
         })
     }
-    // let user
-    // try{
-    //     user=await User.findById(req.userData.userId)
-    // }
-    // catch(err){
-    //     console.log(err)
-    //     const error= new httpError("Creating Tour Failed, Please Try again later",500)
-    //     return next(error)
-    // }
-    // if(!user){
-    //     const error= new httpError("Could Found User for That ID",500)
-    //     return next(error)
-    // }
-    // try{
-    //     const sess=await mongoose.startSession();
-    //     await sess.startTransaction();
-    //     await createTour.save({session:sess});
-    //     user.tours.push(createTour);
-    //     await user.save({session:sess});
-    //     sess.commitTransaction();
-    // }
-    // catch (err){
-    //     console.log(err)
-    //     const error=new httpError("Created Placess failed, Please Try Again Later",500)
-    //     return next(error)
-    // }
     res.status(200)
     res.json({
         message:'Tour Created Successfully',
@@ -113,83 +129,40 @@ const createTour=async (req,res,next)=>{
     })
 }
 const updateParticularTour=async (req,res,next)=>{
-    const {title, description}=req.body
-    const error=validationResult(req)
-    if(!error.isEmpty()){
-        const error= new httpError("Invalid Input Passed, Please Check your Data",422)
-        return next(error)
-    }
-    const pid=req.params.pid
-    let tour;
+    let tour
     try{
-        tour=await Tour.findById(pid)
+        tour=await Tour.findOneAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        })
+        res.status(200).json({
+            status:'success',
+            tour
+        })
     }
     catch(err){
-        const error=new httpError("Could not Find Tours",500)
-        return next(error)
+        res.status(200).json({
+            status:'fail',
+            message:err
+        })
     }
-    tour.title=title
-    tour.description=description
-    if(req.userData.userId!=tour.creator.toString()){
-        const error=new httpError("You cannot Update this tour",401)
-        return next(error)
-    }
-    try{
-        console.log(tour);
-        tour=await tour.save();
-    }
-    catch(err){
-        console.log(err)
-        const error=new httpError("Something went wrong, Could not Update tour",500)
-        return next(error)
-    }
-    res.status(200).json({
-        message:"Updated Successfully",
-        tour:tour.toObject({getters:true})
-    })
 }
 const deleteParticularTour=async (req,res,next)=>{
     const pid=req.params.pid
     let tour;
     try{
-        tour=await Tour.findById(pid).populate('creator')
+        tour=await Tour.findByIdAndDelete(pid)
+        res.status(200).json({
+            status:'success',
+            data:null
+        })
     }
     catch(err){
-        const error=new httpError("Error in Deleting a Tour",500)
-        return next(error)
+        res.status(400).json({
+            status:'fail',
+            message:"Error in Deleting Tour"
+        })
     }
-    if(!tour){
-        const error=new httpError("Could not Find Tours",500)
-        return next(error)
-    }
-    const imagePath=tour.image
-    if(req.userData.userId!=tour.creator.id){
-        const error=new httpError("You cannot Delete this tour",401)
-        return next(error)
-    }
-    try{
-        console.log(tour);
-        const sess=await mongoose.startSession();
-        await sess.startTransaction();
-        await tour.remove({session:sess});
-        tour.creator.tours.pull(tour);
-        await tour.creator.save({session:sess});
-        await sess.commitTransaction();
-    }
-    catch(err){
-        console.log(err)
-        const error=new httpError("Something went wrong, Could not Delete tour",500)
-        return next(error)
-    }
-
-    fs.unlink(imagePath,err=>{
-        console.log(err)
-    })
-
-    res.status(200).json({
-        message:"Deleted Successfully",
-        tour:tour.toObject({getters:true})
-    })
 }
 
 exports.getAllTours=getAllTours
